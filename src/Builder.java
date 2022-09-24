68e1b5882b586bba43c63d89fee22e9a3429afec
Andrii <107118319+EpileptikKid@users.noreply.github.com>
2022-09-24 16:41:05 +0300

    Delete Builder.java

--- src/Builder.java
+++
@@ -1,9 +0,0 @@
-package Builder;
-
-public interface Builder {
-    void reset();
-    void setSeats(int count);
-    void setEngine(String engineName);
-    void setTripComputer(String tripComputerName);
-    void setGPS(boolean installed);
-}
