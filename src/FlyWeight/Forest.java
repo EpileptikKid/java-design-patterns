d9325c98ee59814b2ab7c4675bd905dbf4463913
Andrii <107118319+EpileptikKid@users.noreply.github.com>
2022-09-26 16:13:43 +0300

    Create FlyWeight

---
+++ src/FlyWeight/Forest.java
@@ -0,0 +1,24 @@
+package FlyWeight;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.Graphics;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Forest extends JFrame {
+    private List<Tree> trees = new ArrayList<>();
+
+    public void plantTree(int x, int y, String name, Color color, String otherTreeData) {
+        TreeType treeType = TreeFactory.getTreeType(name, color, otherTreeData);
+        Tree tree = new Tree(x, y, treeType);
+        trees.add(tree);
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        for (Tree tree : trees) {
+            tree.draw(g);
+        }
+    }
+}
